9e5298e63b06541b6f648bdaddf16b15b5852497
sergey_timonin <timonin-93@mail.ru>
2022-10-26 22:36:57 +0300

    Добавлены решения домашнего задания к третьему семинару

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+string Number = Console.ReadLine();
+
+int[] NumMassive = new int[Number.Length];
+int count = 0;
+
+if (Number.Length < 3)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+
+else
+{
+    while (count < Number.Length)
+    {
+        NumMassive[count] = int.Parse(Number[count].ToString());
+        count++;
+    }
+int Answer = NumMassive[2];
+Console.WriteLine("Третье число равно");
+Console.WriteLine(Answer);
+}
+
+// Примечание: задача решена до просмотра семинара, использован способ решения из внешнего источника
